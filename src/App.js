38289249c4ab2b06b49173d8516ebd214a9a5802
Mr Whipp <dwhipp88@gmail.com>
2022-09-20 23:41:28 -0600

    installed react icons library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { BiArchive } from 'react-icons/bi';
+
 function App() {
   return (
     <div className="App">
-      <h1>I'm listening.</h1>
+      <h1>
+        <BiArchive />
+        I'm listening.
+      </h1>
     </div>
   );
 }
